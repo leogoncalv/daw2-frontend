0dbd63fbb4a50d1a71f589ba1a18d184629105c7
leogoncalv <goncalves.leandro@academico.ifpb.edu.br>
2024-08-26 21:42:48 -0300

    fiz

--- src/Components/PainelLoginCadastro/index.js
+++ src/Components/PainelLoginCadastro/index.js
@@ -1,18 +1,28 @@
 import React from "react"
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
 
-function  PainelLoginCadastro (){
+function PainelLoginCadastro() {
 
     return (
-        
-        <div>
-           
-           Login e Cadastro
-           
-        </div>
-        
-        
 
-    ); 
+        <div>
+
+            <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+            <TextField id="filled-basic" label="Filled" variant="filled" />
+            <TextField id="standard-basic" label="Standard" variant="standard" />
+
+
+            <Button variant="contained" disableElevation>
+                Disable elevation
+            </Button>
+
+        </div>
+
+
+
+    );
 
 
 }
